a7f8cc6bb646d940f11c3e9b0f6f5ff0c43c661a
Kaπ <89849620+K4-pi@users.noreply.github.com>
2025-12-07 17:11:41 +0100

    split lines

--- Shell/src/csh.c
+++ Shell/src/csh.c
@@ -2,11 +2,12 @@
 
 #include "stdio.h"
 #include "csh.h"
-
-
+#include "csh_read_line.h"
+#include "csh_split_line.h"
 
 void csh_loop(void) {
     char *line;
+    char **args;
     int status = 1;
 
     do {
@@ -14,7 +15,15 @@ void csh_loop(void) {
 
         line = csh_read_line();
         printf("line: %s\n", line);
+
+        args = csh_split_line(line);
+
+        for (int i = 0; args[i] != NULL; i++) {
+            printf("args[%d]: %s\n", i, args[i]);
+        }
+
         free(line);
+        free(args);
     } while (status);
     
 }
--- Shell/src/csh_split_line.c
+++ Shell/src/csh_split_line.c
@@ -11,13 +11,29 @@ char **csh_split_line(char *line) {
     int index = 0;
 
     char *token;
-    char **tokens = calloc(buffer_size, sizeof(char));
+    char **buffer = calloc(buffer_size, sizeof(char*));
 
-    if (tokens == NULL) {
+    if (buffer == NULL) {
         fprintf(stderr, "csh: allocation error! (csh_split_line.c : 17)\n");
+        exit(EXIT_FAILURE);
     }
 
     token = strtok(line, CSH_TOK_DELIM);
-    
+    while (token != NULL) {
+        buffer[index] = token;
+        index++;
 
+        if (index >= buffer_size) {
+            buffer_size += CSH_BUFSIZE_MOD;
+            buffer = realloc(buffer, sizeof(char*));
+
+            if (buffer == NULL) {
+                fprintf(stderr, "csh: reallocation error! (csh_split_line.c : 31)\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        token = strtok(NULL, CSH_TOK_DELIM);
+    }
+    buffer[index] = NULL;
+    return buffer;
 }
