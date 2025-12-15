fdc56a469f6486ebc06c30fa8d732e66f8d8bcc0
Kaπ <89849620+K4-pi@users.noreply.github.com>
2025-12-15 20:38:14 +0100

    actual fix
    removed wpid which was used uninitialized

--- Shell/src/csh.c
+++ Shell/src/csh.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#include "stdio.h"
 #include "csh.h"
+#include "csh_execute.h"
 #include "csh_read_line.h"
 #include "csh_split_line.h"
-#include "csh_execute.h"
+#include "stdio.h"
 
 int lsh_execute(char **args);
 
@@ -15,20 +15,18 @@ void csh_loop(void) {
     char **args;
     int status;
 
-    do {
-        cwd = csh_get_working_directory();
-        printf("%s > ", cwd);
-        
-        line = csh_read_line();
-        args = csh_split_line(line);
-        
-        status = csh_execute(args);
-        printf("\n");
-        
-        free(cwd);
-        free(line);
-        free(args);
-    } while (status);    
+  do {
+      cwd = csh_get_working_directory();
+      printf("%s > ", cwd);
+
+      line = csh_read_line();
+      args = csh_split_line(line);
+
+      status = csh_execute(args);
+      printf("\n");
+
+      free(cwd);
+      free(line);
+      free(args);
+    } while (status);
 }
-
-
--- Shell/src/csh_run_process.c
+++ Shell/src/csh_run_process.c
@@ -5,22 +5,21 @@
 #include <unistd.h>
 
 int csh_run_process(char **args) {
-    pid_t pid, wpid;
+    pid_t pid;
     int status;
-    
+
     pid = fork();
     if (pid == 0) {
-        //Child
+        // Child
         execvp(args[0], args);
-    }
-    else if (pid > 0) {
-        //Parent
+    } else if (pid > 0) {
+        // Parent
         do {
-            wpid = waitpid(wpid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
-    else {
-        perror("csh: error while running process!\n");
-    }
+            waitpid(pid, &status, WUNTRACED);
+        } 
+        while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        } else {
+            perror("csh: error while running process!\n");
+        }
     return 1;
 }
